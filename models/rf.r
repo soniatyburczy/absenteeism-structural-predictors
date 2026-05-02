641250784a8bb67aeb4dbf549bba0402d5837149
physimcs <styburczy.business@gmail.com>
2026-05-02 14:40:34 -0400

    finalized rf.r w/ shap

--- models/rf.r
+++ models/rf.r
@@ -1,11 +1,15 @@
 library(tidyverse)
 library(tidymodels)
 library(ranger)
+library(treeshap)
 source("scripts/utils.R")
 
 train_df <- read_csv("data/processed/train.csv")
 tune_df <- read_csv("data/processed/tune.csv")
 test_df <- read_csv("data/processed/test.csv")
+
+predictors <- c("Economic Need Index", "% Poverty", 
+                "% English Language Learners", "% Students with Disabilities")
 
 folds <- vfold_cv(train_df, v = 5)
 rf_recipe <- recipe(`% Chronically Absent` ~ `Economic Need Index` + `% Poverty` + 
@@ -29,8 +33,19 @@ tune_res <- tune_grid(
 )
 
 best_params <- select_best(tune_res, metric = "rmse")
-rf_final <- finalize_workflow(best_params)
+rf_final <- finalize_model(rf_tune, best_params)
 
 rf_fit <- rf_final |>
   set_engine("ranger") |>
   fit(rf_recipe, data = train_df)
+
+splits <- list(train = train_df, tune = tune_df, test = test_df)
+rf_results <- evaluate_splits(rf_fit, splits, n_predictors = 4, model_name = "rf")
+write_csv(rf_results, "data/model_data/rf_results.csv")
+
+ranger_model <- extract_fit_engine(rf_fit)
+unified_model <- ranger.unify(ranger_model, data = train_df[predictors])
+treeshap_results <- treeshap(unified_model, x = test_df[predictors])
+
+plot_feature_importance(treeshap_results)
+plot_contribution(treeshap_results, obs = 1)
