da91e4835cf756c6a43770068b03b826810e4a94
physimcs <styburczy.business@gmail.com>
2026-05-01 22:17:48 -0400

    Added graphs.r

--- models/poly.r
+++ models/poly.r
@@ -4,9 +4,28 @@ library(tidyverse)
 train_df <- read_csv("data/processed/train.csv")
 tune_df <- read_csv("data/processed/tune.csv")
 test_df <- read_csv("data/processed/test.csv")
+splits <- list(train = train_df, tune = tune_df, test = test_df)
+results <- list()
 
 for (j in 1:10){
   mod <- lm(`% Chronically Absent` ~ poly(`Economic Need Index`, j, raw=TRUE) + 
   `% Poverty` + `% English Language Learners` + 
   `% Students with Disabilities`, data = train_df)
+  
+  results[[j]] <- evaluate_splits(mod, splits, n_predictors = j + 3, model_name = paste0("poly_", j))
 }
+
+poly_results <- dplyr::bind_rows(results)
+write_csv(poly_results, "data/model_data/poly_results.csv")
+
+best_degree <- poly_results |>
+  filter(split == "tune") |>
+  slice_min(rmse, n = 1) |>
+  pull(model_name) |>
+  str_remove("poly_") |>
+  as.integer()
+
+best_test_result <- poly_results |>
+  filter(split == "test", model_name == paste0("poly_", best_degree))
+
+write_csv(best_test_result, "data/model_data/poly_best_degree.csv")
