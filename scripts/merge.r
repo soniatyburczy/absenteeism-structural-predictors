dfcede391fea41280a4d04d9c67f0cba3d310e56
physimcs <styburczy.business@gmail.com>
2026-05-02 15:55:05 -0400

    small naming changes to merge2.r

--- scripts/merge.r
+++ scripts/merge.r
@@ -1,17 +1,22 @@
 library(tidyverse)
 
 attendance_df <- read_csv("data/raw/attendance.csv", show_col_types = FALSE, locale = locale(grouping_mark = ","))
-demographics_df <- read_csv("data/raw/demographics.csv", show_col_types = FALSE, locale = locale(grouping_mark = ","))
+pr_16 <- read_csv("data/raw/performance.csv", show_col_types = FALSE, locale = locale(grouping_mark = ","))
 
 merged_df <- attendance_df |>
-  filter(Year != "2018-19") |>
-  inner_join(demographics_df, by = c("DBN", "Year")) |>
-  select(-Borough, -District, -`School Name.y`) |>
-  rename(`School Name` = `School Name.x`) |>
-  filter(Grade == "All Grades", `Demographic Variable` == "All Students") |>
-  mutate(
-    across(starts_with("%"), ~ as.numeric(gsub("%", "", .))),
-    `Economic Need Index` = as.numeric(gsub("%", "", `Economic Need Index`))
-  )
+  rename(
+    total_days = `# Total Days`,
+    days_absent = `# Days Absent`,
+    days_present = `# Days Present`,
+    percent_attend = `% Attendance`,
+    `20plus_days` = `# Contributing 20+ Total Days`,
+    chronic_absent = `# Chronically Absent`,
+    percent_chronic = `% Chronically Absent`
+  ) |>
+  mutate(across(starts_with("%"), ~ as.numeric(gsub("%", "", .)))) |>
+  group_by(DBN) |>
+  summarise(across(c(total_days, days_absent, days_present, percent_attend, `20plus_days`, chronic_absent, percent_chronic), mean, na.rm = TRUE)) |>
+  inner_join(pr_16, by = c("DBN" = "dbn"))
 
-write_csv(merged_df, "data/processed/merged.csv")
+merged_df <- na.omit(merged_df)
+write_csv(merged_df, "data/processed/merged2.csv")
